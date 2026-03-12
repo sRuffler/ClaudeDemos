17557c6e7ac6ff76e79586cd20efcca97e897aaa
stefan_ruffler <s.ruffler@mmr-research.com>
2026-03-12 12:50:52 +0000

    refactor: standardize quotation marks and formatting in main.js

--- habit-tracker/src/main.js
+++ habit-tracker/src/main.js
@@ -1,29 +1,29 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import PrimeVue from 'primevue/config'
-import Aura from '@primeuix/themes/aura'
-import ConfirmationService from 'primevue/confirmationservice'
-import ToastService from 'primevue/toastservice'
-import 'primeicons/primeicons.css'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import PrimeVue from "primevue/config";
+import Aura from "@primeuix/themes/aura";
+import ConfirmationService from "primevue/confirmationservice";
+import ToastService from "primevue/toastservice";
+import "primeicons/primeicons.css";
 
-import router from './router/index.js'
-import App from './App.vue'
-import './assets/main.css'
+import router from "./router/index.js";
+import App from "./App.vue";
+import "./assets/main.css";
 
-const app = createApp(App)
-const pinia = createPinia()
+const app = createApp(App);
+const pinia = createPinia();
 
-app.use(pinia)
+app.use(pinia);
 app.use(PrimeVue, {
   theme: {
     preset: Aura,
     options: {
-      darkModeSelector: '.dark-mode',
+      darkModeSelector: ".dark-mode",
     },
   },
-})
-app.use(ConfirmationService)
-app.use(ToastService)
-app.use(router)
+});
+app.use(ConfirmationService);
+app.use(ToastService);
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
